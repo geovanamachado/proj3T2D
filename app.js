515a18249cb8dde0039bfe4fcdc2d536d725ac9d
geovanamachado <luanarosa28143@gmail.com>
2024-10-15 11:43:57 +0000

    alte

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ function viraCartao() {
     respostaEstaVisivel = !respostaEstaVisivel 
     cartao.classList.toggle('active', respostaEstaVisivel)
 }
-cartao.addEventListener( 'click', viraCartao)
+cartao.addEventListener('click', viraCartao)
 
 
-container.appendChild (cartao)
+container.appendChild(cartao)
 
 }
