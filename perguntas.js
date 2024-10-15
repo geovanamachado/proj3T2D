d0caab342f4eb84e60e6185b03c58ec0b49c4902
geovanamachado <luanarosa28143@gmail.com>
2024-10-15 11:33:15 +0000

    alterações

--- perguntas.js
+++ perguntas.js
@@ -4,19 +4,19 @@ criaCartao(
     'O Python é uma linguagem de programação'
 )
 
-    criaCartao(
-    'Geografia'
+criaCartao(
+    'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
-    criaCartao(
+criaCartao(
     'Programação' ,
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
     )
 
-    criaCartao(
+criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
